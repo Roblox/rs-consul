2251deaa74670e8007e43f453f49785063903072
x0rw <mahdi.svt5@gmail.com>
2025-05-10 22:43:03 +0100

    Add list_acl_policies(), create_acl_policy(), and examples

--- examples/get_acl_tokens.rs
+++ examples/get_acl_tokens.rs
@@ -4,7 +4,8 @@ use rs_consul::{Config, Consul};
 async fn main() {
     let consul_config = Config {
         address: "http://localhost:8500".to_string(),
-        token: Some(String::from("8fc9e787-674f-0709-cfd5-bfdabd73a70d")), // use bootstraped token
+        token: Some(String::from("8fc9e787-674f-0709-cfd5-bfdabd73a70d")), // use bootstraped
+        // token (with write perm)
         ..Default::default()
     };
     let consul = Consul::new(consul_config);
--- src/acl.rs
+++ src/acl.rs
@@ -3,11 +3,15 @@ use std::time::Duration;
 use http::Method;
 use http_body_util::combinators::BoxBody;
 use http_body_util::Empty;
+use http_body_util::Full;
 use hyper::body::Bytes;
 
 use crate::errors::ConsulError;
+use crate::types::ACLPolicy;
 use crate::types::ACLToken;
+use crate::types::CreateACLPolicyRequest;
 use crate::Consul;
+use crate::Function;
 use crate::Result;
 use hyper::body::Buf;
 
@@ -26,5 +30,37 @@ impl Consul {
             .await?;
         serde_json::from_reader(response_body.reader())
             .map_err(ConsulError::ResponseDeserializationFailed)
+    }
+
+    /// list acl policies
+    pub async fn list_acl_policies(&self) -> Result<Vec<ACLPolicy>> {
+        let uri = format!("{}/v1/acl/policies", self.config.address);
+        let request = hyper::Request::builder().method(Method::GET).uri(uri);
+        let (response_body, _index) = self
+            .execute_request(
+                request,
+                BoxBody::new(Empty::<Bytes>::new()),
+                Some(Duration::from_secs(5)),
+                crate::Function::ListACLPolicies,
+            )
+            .await?;
+        serde_json::from_reader(response_body.reader())
+            .map_err(ConsulError::ResponseDeserializationFailed)
+    }
+
+    /// Create an ACL policy
+    /// https://developer.hashicorp.com/consul/api-docs/acl/policies
+    pub async fn create_acl_policy(&self, payload: &CreateACLPolicyRequest) -> Result<()> {
+        let uri = format!("{}/v1/acl/policy", self.config.address);
+        let request = hyper::Request::builder().method(Method::PUT).uri(uri);
+        let payload = serde_json::to_string(payload).map_err(ConsulError::InvalidRequest)?;
+        self.execute_request(
+            request,
+            BoxBody::new(Full::<Bytes>::new(Bytes::from(payload.into_bytes()))),
+            Some(Duration::from_secs(5)),
+            Function::CreateACLPolicy,
+        )
+        .await?;
+        Ok(())
     }
 }
